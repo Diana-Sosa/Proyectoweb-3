4c5554bcb91cfe53bb4b0955f6e83730a2167dd9
Diana-Sosa <dvsosad02@gmail.com>
2024-12-02 14:01:27 -0600

    Add some changes

--- banco-recursos-academicos/src/App.js
+++ banco-recursos-academicos/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./components/auth";  // Asegúrate de que el archivo se llama Auth.js con 'A' mayúscula
+import AddResource from "./components/AddResource";
+import ResourceList from "./components/ResourceList";
 
 function App() {
+  const [user, setUser] = useState(null);  // Estado para almacenar la información del usuario autenticado
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h1>Bienvenido a Banco de Recursos Académicos</h1>
+        {/* Aquí puedes mostrar el estado del usuario */}
+        {user ? (
+          <p>Bienvenido, {user.displayName}</p>
+        ) : (
+          <p>No has iniciado sesión.</p>
+        )}
+        
+        <Routes>
+          {/* Rutas del proyecto */}
+          <Route path="/" element={<ResourceList />} />
+          <Route path="/auth" element={<Auth setUser={setUser} />} />
+          <Route path="/add" element={<AddResource />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
